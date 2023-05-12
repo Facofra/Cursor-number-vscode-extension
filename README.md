651d0e7e17406134c7e488c021f5612b1798c009
Facofra <facundo_olleros@hotmail.com>
2023-05-12 19:08:43 -0300

    added creating lines with number and or text

--- README.md
+++ README.md
@@ -2,9 +2,39 @@
 
 ## Features
 
-Right click -> Cursor number
-Insert the relative line number in your text file.
-Works with multi cursor or highlighting multi line text.
+Right click -> Cursor number<br />
+Insert the relative line number in your text file.<br />
+Works with multi cursor , highlighting multi line text, or on a single empty line.<br />
+
+Multi cursor:
+    - inserts relative line number to each cursor position
+
+Highliting multi line text:
+    - inserts relative line number to the start of each highlited line
+
+Single empty line:
+    - An input prompt will appear, where you can insert three different values separated by comma:<br />
+        1. Number of lines to create<br />
+        2. [t|f] true or false if you want to insert relative line numbers on that lines<br />
+        3. \<text> text which you want to insert on those lines<br />
+
+    example input: 3,t,test 
+    will insert:
+        1 test
+        2 test
+        3 test
+
+    example input: 3
+    will insert:
+        1
+        2
+        3
+
+    example input: 3,f,test
+    will insert:
+        test
+        test
+        test
 
 
 ## Release Notes
--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,21 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+function obtenerValores(value:string) {
+	const valores = value.split(",");
+	if (valores.length <= 2) {
+		return valores;
+	}
+	
+	const firstTwo = valores.splice(0, 2);
+	const result = [...firstTwo, valores.join(",")];
+	return result;
+
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('Congratulations, your extension "cursornumber" is now active!');
 
 	let disposable = vscode.commands.registerCommand('cursornumber.cursorNumber', () => {
 		// The code you place here will be executed every time your command is executed
@@ -17,9 +27,8 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		
 		const selections = editor.selections;
-		const selection = editor.selection;
-
 		
+		// Si hay multicursor
 		if (selections.length > 1) {
 			editor.edit((editBuilder) => {
 				editor.selections.forEach((selection, index) => {
@@ -32,14 +41,62 @@ export function activate(context: vscode.ExtensionContext) {
 			});
 			return;
 		}
+
 		
-		editor.edit(editBuilder => {
+		const selection = editor.selection;
+		const cursorLine = editor.document.lineAt(selection.active.line);
+
+		// si hay seleccion, o una linea no vacia
+		if(! selection.start.isEqual(selection.end) || cursorLine.text.length != 0  ){
+
+			editor.edit(editBuilder => {
+				let lineNumber=1;
+				for (let i = selection.start.line; i <= selection.end.line; i++) {
+					editBuilder.insert(new vscode.Position(i, 0), `${lineNumber} `);
+					lineNumber++;
+				}
+			});
+			return;
+		}
+
+
+
+		
+		
+		// si el cursor esta en una linea vacia y no hay seleccion
+		vscode.window.showInputBox({
+			prompt: "n° de líneas a insertar,(t|f) insertamos número de línea?,texto",
+			placeHolder: "10,t,ejemplo",
+			value: ""
+		}).then(function (value) {
+			let valores = obtenerValores(`${value}`);
+			
+			let numberOfLines = parseInt(valores[0]) || 0;
+			let printNumbers = valores[1] || 't';
+			let texto = valores[2] || '';
+
 			let lineNumber=1;
-			for (let i = selection.start.line; i <= selection.end.line; i++) {
-				editBuilder.insert(new vscode.Position(i, 0), `${lineNumber} `);
+			let textToInsert ="";
+			for (let i = selection.start.line; i < selection.start.line + numberOfLines; i++) {
+				if (printNumbers.toLowerCase() == 't') {
+					textToInsert += `${lineNumber} `;
+				}
+				
+				if (texto != '') {
+					textToInsert += texto;
+				}
+
+				textToInsert += `\n`;
 				lineNumber++;
 			}
+			
+			editor.edit(editBuilder => {
+				editBuilder.insert(selection.start, textToInsert);
+			});
+
 		});
+
+
 
 
 	});
