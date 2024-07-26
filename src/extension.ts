511ae95ac6ef46311892ea0dd7966fca2d43288f
Facofra <facundo_olleros@hotmail.com>
2024-07-26 18:21:15 -0300

    addes copying x numbes of time a line with text

--- src/extension.ts
+++ src/extension.ts
@@ -28,7 +28,7 @@ export function activate(context: vscode.ExtensionContext) {
 		
 		const selections = editor.selections;
 		
-		// Si hay multicursor
+		// Si hay multicursor, en cada cursos poner numero de cursor
 		if (selections.length > 1) {
 			editor.edit((editBuilder) => {
 				editor.selections.forEach((selection, index) => {
@@ -46,8 +46,8 @@ export function activate(context: vscode.ExtensionContext) {
 		const selection = editor.selection;
 		const cursorLine = editor.document.lineAt(selection.active.line);
 
-		// si hay seleccion, o una linea no vacia
-		if(! selection.start.isEqual(selection.end) || cursorLine.text.length != 0  ){
+		// si hay seleccion, indicar numero al principio de cada linea
+		if(! selection.start.isEqual(selection.end) ){
 
 			editor.edit(editBuilder => {
 				let lineNumber=1;
@@ -59,42 +59,71 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
+		// si el cursor está en una linea no vacía, pedir cuantas copias quiero
+		if(cursorLine.text.length != 0){
+			vscode.window.showInputBox({
+				prompt: "n° de copias requeridas",
+				placeHolder: "10",
+				value: ""
+			}).then(function (value:string='1') {
+				const numeroDeCopias = parseInt(value);
+				const lineText = cursorLine.text;
+				
+				let textToInsert='';
+				for (let i = 1; i < numeroDeCopias; i++) {
+					textToInsert += `\n${lineText}`;
+				}
 
-
-		
-		
-		// si el cursor esta en una linea vacia y no hay seleccion
-		vscode.window.showInputBox({
-			prompt: "n° de líneas a insertar,(t|f) insertamos número de línea?,texto",
-			placeHolder: "10,t,ejemplo",
-			value: ""
-		}).then(function (value) {
-			let valores = obtenerValores(`${value}`);
-			
-			let numberOfLines = parseInt(valores[0]) || 0;
-			let printNumbers = valores[1] || 't';
-			let texto = valores[2] || '';
-
-			let lineNumber=1;
-			let textToInsert ="";
-			for (let i = selection.start.line; i < selection.start.line + numberOfLines; i++) {
-				if (printNumbers.toLowerCase() == 't') {
-					textToInsert += `${lineNumber} `;
+				// solo copiar si queremos mas de 1 copia
+				if (numeroDeCopias > 1) {
+					editor.edit(editBuilder => {
+						editBuilder.insert(new vscode.Position(selection.active.line, lineText.length), `${textToInsert}`);
+					});
 				}
 				
-				if (texto != '') {
-					textToInsert += texto;
-				}
-
-				textToInsert += `\n`;
-				lineNumber++;
-			}
-			
-			editor.edit(editBuilder => {
-				editBuilder.insert(selection.start, textToInsert);
+	
 			});
+		}
 
-		});
+
+
+		
+		
+		// si el cursor esta en una linea vacia, pedir cuantas lineas insertar, si insertar numero, y texto a agregar
+		if(cursorLine.text.length == 0){
+			vscode.window.showInputBox({
+				prompt: "n° de líneas a insertar,(t|f) insertamos número de línea?,texto",
+				placeHolder: "10,t,ejemplo",
+				value: ""
+			}).then(function (value) {
+				let valores = obtenerValores(`${value}`);
+				
+				let numberOfLines = parseInt(valores[0]) || 0;
+				let printNumbers = valores[1] || 't';
+				let texto = valores[2] || '';
+	
+				let lineNumber=1;
+				let textToInsert ="";
+				for (let i = selection.start.line; i < selection.start.line + numberOfLines; i++) {
+					if (printNumbers.toLowerCase() == 't') {
+						textToInsert += `${lineNumber} `;
+					}
+					
+					if (texto != '') {
+						textToInsert += texto;
+					}
+	
+					textToInsert += `\n`;
+					lineNumber++;
+				}
+				
+				editor.edit(editBuilder => {
+					editBuilder.insert(selection.start, textToInsert);
+				});
+	
+			});
+		
+		}
 
 
 
